38cab70769be1dfa9529024275eea1aca2719b13
Nikolay.Vinokurov <nikolay.vinokurov@ics-it.ru>
2024-04-03 19:50:53 +0300

    Aside opened by click

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,13 +1,23 @@
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
+import { useState } from 'react';
+import classNames from 'classnames';
 
 import styles from './ArticleParamsForm.module.scss';
 
 export const ArticleParamsForm = () => {
+	const [open, setOpen] = useState(false);
+
+	const toggleAside = () => setOpen(true);
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
+			<ArrowButton onClick={toggleAside} />
+			<aside
+				className={classNames(
+					styles.container,
+					open ? styles.container_open : ''
+				)}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
