012cca450f4323704312974b64815ad970eea3fc
Nikolay.Vinokurov <nikolay.vinokurov@ics-it.ru>
2024-04-07 21:06:26 +0300

    Corrected comments #1

---
+++ src/components/app/App.tsx
@@ -0,0 +1,33 @@
+import { CSSProperties, useState } from 'react';
+
+import { Article } from '../../components/article/Article';
+import { ArticleParamsForm } from '../../components/article-params-form/ArticleParamsForm';
+import {
+	defaultArticleState,
+	ArticleStateType,
+} from '../../constants/articleProps';
+
+import '../../styles/index.scss';
+import styles from './App.module.scss';
+
+export const App = () => {
+	const [articleState, setArticleState] =
+		useState<ArticleStateType>(defaultArticleState);
+
+	return (
+		<main
+			className={styles.main}
+			style={
+				{
+					'--font-family': articleState.fontFamilyOption.value,
+					'--font-size': articleState.fontSizeOption.value,
+					'--font-color': articleState.fontColor.value,
+					'--container-width': articleState.contentWidth.value,
+					'--bg-color': articleState.backgroundColor.value,
+				} as CSSProperties
+			}>
+			<ArticleParamsForm setPageState={setArticleState} />
+			<Article />
+		</main>
+	);
+};
